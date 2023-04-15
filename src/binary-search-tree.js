a45a2c3d6464ae1009f6ebc1c86ba835c36a80e7
MarkHW <lolka-pop@rambler.ru>
2023-04-16 00:55:05 +0300

    feat: finally end of data structers

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,153 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
+class Node {
+	constructor(data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
+
 class BinarySearchTree {
+	constructor() {
+		this._root = null;
+	}
+	add(value) {
+		let newItem = new Node(value);
+		if (!this._root) {
+			this._root = newItem;
+			return;
+		}
+		let currentItem = this._root;
+		while (currentItem) {
+			if (newItem.data < currentItem.data) {
+				if (!currentItem.left) {
+					currentItem.left = newItem;
+					return;
+				}
+				currentItem = currentItem.left;
+			} else {
+				if (!currentItem.right) {
+					currentItem.right = newItem;
+					return;
+				}
+				currentItem = currentItem.right;
+			}
+		}
+	}
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	root() {
+		if (this._root !== null) {
+			return this._root;
+		}
+		return null;
+	}
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	has(value, node = this._root) {
+		let k = value;
+		if (!node) {
+			return false;
+		}
+		if (node.data === value) {
+			return true;
+		}
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+		return this.has(k, node.left) || this.has(k, node.right);
+	}
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	find(value, node = this._root) {
+		if (!value) {
+			return null;
+		}
+		let k = value;
+		if (!node) {
+			return null;
+		}
+		if (node.data === value) {
+			return node;
+		}
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+		return this.find(k, node.left) || this.find(k, node.right);
+	}
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	min() {
+		let currentItem = this._root;
+		while (currentItem) {
+			if (currentItem.left) {
+				currentItem = currentItem.left;
+			} else {
+				return currentItem.data;
+			}
+		}
+	}
 
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	max() {
+		let currentItem = this._root;
+		while (currentItem) {
+			if (currentItem.right) {
+				currentItem = currentItem.right;
+			} else {
+				return currentItem.data;
+			}
+		}
+	}
+
+	remove(data) {
+		this._root = this.tryRemove(this._root, data);
+	}
+
+	//сложнейшая шляпа:
+	tryRemove(currentNode, data) {
+		if (!currentNode) {
+			return null;
+		}
+
+		if (data === currentNode.data) {
+			if (!currentNode.left && !currentNode.right) {
+				//Лист
+				return null;
+			}
+			if (!currentNode.left) {
+				//левого
+				return currentNode.right;
+			}
+			if (!currentNode.right) {
+				//нет правого
+				return currentNode.left;
+			}
+
+			//два дочерних узла
+			const tempNode = this.getMinimum(currentNode.right);
+			currentNode.data = tempNode.data;
+			currentNode.right = this.tryRemove(currentNode.right, tempNode.data);
+			return currentNode;
+		}
+
+		if (data < currentNode.data) {
+			//ищем в левом поддереве
+			currentNode.left = this.tryRemove(currentNode.left, data);
+			return currentNode;
+		}
+
+		//ищем в правом поддереве
+		currentNode.right = this.tryRemove(currentNode.right, data);
+		return currentNode;
+	}
+
+	getMinimum(currentNode) {
+		while (currentNode.left) {
+			currentNode = currentNode.left;
+		}
+		return currentNode;
+	}
 }
 
 module.exports = {
-  BinarySearchTree
+	BinarySearchTree,
 };
